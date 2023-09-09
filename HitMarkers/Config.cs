5c30d021c906dcb0b4c25860c1407b24bf78ff43
aalonee <alone@evolution-project.online>
2023-09-09 22:17:51 +0300

    Добавьте файлы проекта.

---
+++ HitMarkers/Config.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace killpopup
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        public bool Debug { get; set; } = false;
+
+        [Description("Use the %player% placeholder")]
+        public string KillMessage { get; set; } = "<b>Killed <color=red>%player%</color></b>";
+
+        [Description("Duration of the hint")]
+        public float HintDuration { get; set; } = 5.0F;
+
+        [Description("Hitmarker hint. %damage% - damage, %remaining% - target's remaining hp")]
+        public string HitmarkerHint { get; set; } = "<color=red>%damage%</color> | <color=blue>%remaining%</color>";
+
+        [Description("Duration of the hitmarker hint (0.2 is recommended)")]
+        public float HitmarkerDuration { get; set; } = 0.2F;
+    }
+}
---
+++ HitMarkers/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.API.Features.DamageHandlers;
+using Exiled.Events.Commands.Reload;
+using Exiled.Events.EventArgs;
+using Exiled.Events.EventArgs.Player;
+using PluginAPI.Events;
+
+namespace killpopup
+{
+    public sealed class Plugin : Plugin<Config>
+    {
+        public override PluginPriority Priority { get; } = PluginPriority.Default;
+        public override string Name { get; } = "Hitmarkers";
+
+        public override string Author { get; } = "alone";
+
+        public override string Prefix { get; } = "Hitmarkers";
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();   
+            Exiled.Events.Handlers.Player.Dying += DyingEvent;
+            Exiled.Events.Handlers.Player.Hurting += OnHurting;
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+            Exiled.Events.Handlers.Player.Dying -= DyingEvent;
+            Exiled.Events.Handlers.Player.Hurting -= OnHurting;
+        }
+
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            if (ev.Attacker != null && ev.Player != null && ev.Attacker != ev.Player)
+            {
+                ev.Attacker.ShowHint(Config.HitmarkerHint.Replace("%damage%", Math.Round(ev.Amount).ToString()).Replace("%remaining%", (Math.Round(ev.Player.Health) - Math.Round(ev.Amount)).ToString()), Config.HitmarkerDuration);
+            }
+        }
+
+        public void DyingEvent(DyingEventArgs ev)
+        {
+            if (ev.Attacker != null && ev.Player != null && ev.Attacker != ev.Player)
+            {
+                ev.Attacker.ShowHint(Config.KillMessage.Replace("%player%", ev.Player.Nickname), Config.HintDuration);
+            }
+        }
+    }
+}
